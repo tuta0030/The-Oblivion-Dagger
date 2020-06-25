9cdbef151a7074418ccda624be2c28dc56d814cc
tuta0030 <jokerisfun@126.com>
2020-06-25 19:06:53 +0800

    更新保存方法

--- main.py
+++ main.py
@@ -20,6 +20,7 @@
 
 import re
 import os
+import inspect
 
 PATTERN_EACH_CHARACTER = re.compile('姓名-.+?(?=姓名-)', re.DOTALL)
 PATTERN_EACH_CHARACTER_NAME = re.compile('(?<=姓名-).+(?=[：:])')
@@ -39,22 +40,13 @@ def load_all_characters_info(which_file: str) -> dict:
 
 class Character:
 
-    def __init__(self, name, brief, species, *relations):
+    def __init__(self, name, brief, species, **kwargs):
         self.name = name
         self.brief = brief
         self.species = species
-        self.relations = relations
-        pass
-
-    def showinfo(self):
-        print('名字:\t' + self.name)
-        print('简介:\t' + self.brief)
-        print('种族:\t' + self.species)
-        print('关系:\t', end='')
-        print(self.relations)
-
-    def addinfo(self, info_type, content, **args):
-        pass
+        self.relations = kwargs.get('relations', None)
+        self.pet = kwargs.get('pet', None)
+        self.age = kwargs.get('age', None)
 
     def load_info(self, who: str):
         char_dict = load_all_characters_info('角色')
@@ -62,20 +54,18 @@ class Character:
         print(char_dict[who][1])
 
     def save_character(self):
+        attributes = inspect.getmembers(self)
         with open(PATH_CHARACTERS_OBJ+self.name+'.py', 'w', encoding='utf-8') as f:
-            f.write('name = '+f'\'{self.name}\'')
-            f.write('\n')
-            f.write('brief = ' + f'\"\"\"{self.brief}\"\"\"')
-            f.write('\n')
-            f.write('species = '+f'\'{self.species}\'')
-            f.write('\n')
-            f.write('relations = ' + f'\"\"\"{str(self.relations)}\"\"\"')
-            f.write('\n')
+            for att in attributes:
+                if not (att[0].startswith('__') and att[0].endswith('__')):
+                    if not inspect.ismethod(self.__getattribute__(att[0])):
+                        f.write(f'{att[0]} = "{att[1]}"')
+                        f.write('\n')
+                        print(att)
 
     def delete_character(self):
         os.remove(PATH_CHARACTERS_OBJ+self.name+'.py')
 
 
-taran = Character('taran', 'None', 'greeling', '21', {'lover': 'tuna', 'parents': 'Tarlonarr, ?'})
-taran.load_info('塔伦')
-
+taran = Character('taran', 'None', 'greeling', relations={'lover': 'tuna'}, age='25')
+taran.save_character()
