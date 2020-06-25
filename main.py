4de912fb40b52103015a14d34a09904dc4065be4
tuta0030 <jokerisfun@126.com>
2020-06-25 19:33:16 +0800

    重新开始

--- main.py
+++ main.py
@@ -7,14 +7,16 @@
 
 """
 
-# 从角色简介中获取角色简介信息
-# 通过选择需要编辑的角色相关文件，编辑对应文件
-# GUI 界面展示角色相关信息， 包括图片，简介，描述，主线故事，通过点击关联人物可以跳转
+故事生成器
 
-1. 学习tkinter创建自定义图形的方法
-2. 设计UI
-3. 绘制UI
-4. 添加
+1. 平淡的开头
+2. 遇到了问题
+3. 寻找解决办法
+4. 找到解决办法
+5. 付出严重代价
+6. 勉强击败敌人
+7. 改变了自己
+8. 回到了平淡的生活
 
 """
 
@@ -22,50 +24,40 @@ import re
 import os
 import inspect
 
-PATTERN_EACH_CHARACTER = re.compile('姓名-.+?(?=姓名-)', re.DOTALL)
-PATTERN_EACH_CHARACTER_NAME = re.compile('(?<=姓名-).+(?=[：:])')
-PATH_CHARACTERS_OBJ = os.curdir+'\\Characters\\'
 
+class StoriesGenerator(object):
 
-def load_all_characters_info(which_file: str) -> dict:
-    char_dict = {}
-    raw_content = open(which_file, 'r', encoding='utf-8').read()
-    _all = re.findall(PATTERN_EACH_CHARACTER, raw_content)
-    index = 0
-    for i in _all:
-        char_dict[re.findall(PATTERN_EACH_CHARACTER_NAME, i)[0]] = [index, i]
-        index += 1
-    return char_dict
+    def __init__(self, who, where, problem, how, boss, solution):
+        self.who = who
+        self.where = where
+        self.problem = problem
+        self.how = how
+        self.boss = boss
+        self.solution = solution
 
+    def mundane_start(self):
+        pass
 
-class Character:
+    def encounter_problem(self):
+        pass
 
-    def __init__(self, name, brief, species, **kwargs):
-        self.name = name
-        self.brief = brief
-        self.species = species
-        self.relations = kwargs.get('relations', None)
-        self.pet = kwargs.get('pet', None)
-        self.age = kwargs.get('age', None)
+    def finding_solution(self):
+        pass
 
-    def load_info(self, who: str):
-        char_dict = load_all_characters_info('角色')
-        self.brief = char_dict[who][1]
-        print(char_dict[who][1])
+    def find_the_solution(self):
+        pass
 
-    def save_character(self):
-        attributes = inspect.getmembers(self)
-        with open(PATH_CHARACTERS_OBJ+self.name+'.py', 'w', encoding='utf-8') as f:
-            for att in attributes:
-                if not (att[0].startswith('__') and att[0].endswith('__')):
-                    if not inspect.ismethod(self.__getattribute__(att[0])):
-                        f.write(f'{att[0]} = "{att[1]}"')
-                        f.write('\n')
-                        print(att)
+    def defeated_enemy(self):
+        pass
 
-    def delete_character(self):
-        os.remove(PATH_CHARACTERS_OBJ+self.name+'.py')
+    def pay_a_great_price(self):
+        pass
 
+    def change_self(self):
+        pass
 
-taran = Character('taran', 'None', 'greeling', relations={'lover': 'tuna'}, age='25')
-taran.save_character()
+    def back_to_mundane(self):
+        pass
+
+    def main(self):
+        pass
